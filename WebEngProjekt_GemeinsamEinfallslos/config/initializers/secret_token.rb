d3b9b7fe8ff430be350463a92a5cc8d5b2bbcb06
Dissmissed <Dissmissed@users.noreply.github.com>
2014-04-28 11:20:15 +0200

    * New RubyMine-rails project

---
+++ WebEngProjekt_GemeinsamEinfallslos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebEngProjektGemeinsameinfallslos::Application.config.secret_token = '2924b23e181dd7737865f25e84b8167514158a346e1e651e7c637c98b8149cdc394d21db3af408557b847a70f34ed561b1ddc9ba14995d0836e1c912d478aed5'
